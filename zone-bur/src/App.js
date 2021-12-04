6246e8ff17173e7281473964b73f93fbbdca779f
ingHHK <inghyunho@naver.com>
2021-12-04 14:37:49 +0900

    update api

--- zone-bur/src/App.js
+++ zone-bur/src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
+export default class App extends React.Component {
+  async getData() {
+    const res = await axios.get('http://localhost:7071/api/datetimes');
+
+    return res.data.message;
+  }
+
+  constructor(...args) {
+    super(...args);
+    this.state = {data: null};
+  }
+
+  componentDidMount() {
+    if(!this.state.data) {
+      (async () => {
+        try {
+          this.setState({data: await this.getData()});
+        } catch(e) {
+        }
+      })();
+    }
+  }
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +46,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+}
